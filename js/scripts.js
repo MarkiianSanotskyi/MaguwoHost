8a22be2c03c6d4573904c144e1f8f1bc85d1cf17
Markiian Sanotskyi <marksanockiy@gmail.com>
2020-08-14 10:02:14 -0400

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+/*placeholder*/
+$(document).ready(function(){
+        $.Placeholder.init({ color : "#797979" });
+ });
+
+ $(document).ready(function(){
+   $('a[href*=#]').bind("click", function(e){
+      var anchor = $(this);
+      $('html, body').stop().animate({
+         scrollTop: $(anchor.attr('href')).offset().top
+      }, 1400);
+      e.preventDefault();
+   });
+   return false;
+});
